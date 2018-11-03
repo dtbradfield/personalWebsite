3ace8f19b8dd35de5969a8f32c26ae6a02f9a3a0
DonTheChimera <41645869+DonTheChimera@users.noreply.github.com>
2018-11-03 11:53:23 -0500

    clock files now in own folder

---
+++ clockPage/clockScript.js
@@ -0,0 +1,31 @@
+//Using JavaScript Module patterns for this app
+const setClock = ( function() {
+    //Set the clock with hours, mins, and secs
+    
+    let theDate = new Date();
+    let theHours = theDate.getHours();
+    let theMinutes = theDate.getMinutes();
+    let theSeconds = theDate.getSeconds();
+    
+    //Change the text of the div with id "clock"
+    
+    let clockDiv = document.getElementById('clock');
+    clockDiv.textContent = `${theHours}:${theMinutes}:${theSeconds}`;
+
+    //Set a function to change the div clock textContent perpetually, every second
+
+    let tickTock = setInterval( function() {
+        
+        //Assign time again for the tickTock
+        
+        let theDate = new Date();
+        let theHours = theDate.getHours();
+        let theMinutes = theDate.getMinutes();
+        let theSeconds = theDate.getSeconds();
+
+        //Change clock textContent perpetually
+        
+        clockDiv.textContent = `${theHours}:${theMinutes}:${theSeconds}`;
+
+    }, 1000 );
+}());
